75c325b0feafb9f6d5d291a1ad3d00bd74ea2419
Benjamin Santos <aluckyblock9098@gmail.com>
2023-01-26 12:25:43 -0600

    fixed cat file path

--- Lab3/Cat.java
+++ Lab3/Cat.java
@@ -6,13 +6,13 @@ public class Cat extends Actor
     public Cat()
     {
         //Create a MayflowerImage object from the specified image file
-        MayflowerImage p = new MayflowerImage ("img/cat/Walk ().png");
+        MayflowerImage p = new MayflowerImage ("Lab3/img/cat/Walk (1).png");
         // fill frames array
-        for (int i = 1; i <= 10; i++){
-            String filePath = "img/cat/Walk ("+i+")";
-            frames[i-1] = new MayflowerImage(filePath);
-            System.out.println(frames[i-1]);
-        }
+        // for (int i = 1; i <= 10; i++){
+        //     String filePath = "img/cat/Walk ("+i+")";
+        //     frames[i-1] = new MayflowerImage(filePath);
+        //     System.out.println(frames[i-1]);
+        // }
         // Scale this image to 30% of the origianl size
         p.scale(0.3);
         //Set this Actor's image to the MayflowerImage object p
